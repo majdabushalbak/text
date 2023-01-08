8bfbb1ce9450e0b029ee79ba55d666831a4d9cbc
m4 <116095228+majdabushalbak@users.noreply.github.com>
2023-01-08 10:13:32 -1000

    Update code.py

--- code.py
+++ code.py
@@ -4,4 +4,4 @@ print visit[1] # return string : 'noah-'
 print visit[2] # return string : 'chatelain'
 print visit[3] # return string : '.fr'
 
-print 'go visit : ' + visit[1] + visit[2] + visit[3]
+print 'go visit : ' + visit[1] + visit[2] + visit[3]s
