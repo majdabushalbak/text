30186e6f7d1492322356e781aa2b5b9de1a47e95
laithisbaitan <80163581+laithisbaitan@users.noreply.github.com>
2023-01-08 22:15:58 +0200

    made branch

--- code.py
+++ code.py
@@ -4,4 +4,10 @@ print visit[1] # return string : 'noah-'
 print visit[2] # return string : 'chatelain'
 print visit[3] # return string : '.fr'
 
-print 'go visit : ' + visit[1] + visit[2] + visit[3]s
+print 'go visit : ' + visit[1] + visit[2] + visit[3]
+
+
+my_str = 'bobbyhadz.com'
+
+# ✅ print first 5 characters in string
+print(my_str[:5]) 
