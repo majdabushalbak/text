398e03b903dcc9fcc1f97686a92060ae563f7738
m4 <116095228+majdabushalbak@users.noreply.github.com>
2023-01-08 10:02:40 -1000

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 visit = ['noah-', 'chatelain', '.fr']
 
-print visit[0] # return string : 'noah-'
-print visit[1] # return string : 'chatelain'
-print visit[2] # return string : '.fr'
+print visit[1] # return string : 'noah-'
+print visit[2] # return string : 'chatelain'
+print visit[3] # return string : '.fr'
 
-print 'go visit : ' + visit[0] + visit[1] + visit[2]
+print 'go visit : ' + visit[1] + visit[2] + visit[3]
